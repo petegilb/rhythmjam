6f25f865e3d4aede2da1ede94eb31fc9595f0b7d
Peter Gilbert <petegilb@bu.edu>
2026-07-24 14:36:06 +0900

    add spacebar input to check if ok or not, (need to be able to set where in song)

--- main.gd
+++ main.gd
@@ -2,20 +2,25 @@ extends Node2D
 class_name RhythmMain
 
 signal song_beat(last_beat)
+signal enter_beat
+signal exit_beat
+signal input_success
 
 @onready var music_player = $MusicPlayer
 
+const INPUT_MARGIN = 0.08
 var song1: AudioStream = preload("res://music/3.19 Sketch.mp3")
 var bpm = 0
 var beat_duration_ms = 0
 var last_beat = 0
 var next_beat_position = 0
 var current_song_position: float = 0.0
+var active_beat = -1
 
 # referenced this video: https://www.youtube.com/watch?v=9XcLoEVnjrA
 
 func set_song(input_song: AudioStreamMP3) -> void:
-	if not input_song:
+	if not input_song or input_song.bpm <= 0:
 		printerr("input song is invalid! can't set song")
 		return
 	music_player.stream = input_song
@@ -36,11 +41,32 @@ func start_song():
 	music_player.play()
 	
 func update_song():
-	current_song_position = music_player.get_playback_position()
+	current_song_position = get_accurate_position()
 	if current_song_position >= next_beat_position:
 		last_beat += 1
 		song_beat.emit(last_beat)
 		next_beat_position += beat_duration_ms
+	# TODO update this to the song beats (where to press button inputs)	
+	if current_song_position >= next_beat_position - INPUT_MARGIN:
+		if active_beat != next_beat_position:
+			active_beat = next_beat_position
+			enter_beat.emit()
+	elif active_beat > -1 and current_song_position >= active_beat + INPUT_MARGIN:
+		active_beat = -1
+		exit_beat.emit()
+	
+	if Input.is_action_just_pressed("space"):
+		if active_beat != -1:
+			print("ok!")
+			input_success.emit()
+		else:
+			print("miss")
+
+func get_accurate_position() -> float:
+	var playback_pos: float = music_player.get_playback_position()
+	playback_pos += AudioServer.get_time_since_last_mix()
+	playback_pos -= AudioServer.get_output_latency()
+	return playback_pos
 
 func _ready() -> void:
 	set_song(song1)
@@ -48,4 +74,4 @@ func _ready() -> void:
 
 func _process(_delta: float) -> void:
 	if music_player.is_playing():
-		update_song()	
+		update_song()
--- ui/ui.gd
+++ ui/ui.gd
@@ -5,10 +5,26 @@ extends Control
 
 func song_beat_conn(last_beat):
 	print(last_beat)
+	#beat_visual.visible = true
+	#await get_tree().create_timer(main.beat_duration_ms / 2).timeout
+	#beat_visual.visible = false
+
+func input_range_start():
 	beat_visual.visible = true
-	await get_tree().create_timer(main.beat_duration_ms / 2).timeout
+	return
+	
+func input_range_stop():
+	beat_visual.color = Color(0.843, 0.239, 0.173)
 	beat_visual.visible = false
+	return
+	
+func input_success():
+	beat_visual.color = Color(0.0, 0.953, 0.0, 1.0)
+	return
 
 func _ready() -> void:
 	beat_visual.visible = false
 	main.song_beat.connect(song_beat_conn)
+	main.enter_beat.connect(input_range_start)
+	main.exit_beat.connect(input_range_stop)
+	main.input_success.connect(input_success)
