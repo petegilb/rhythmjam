745026ea81c6c7f69a2ad7d01e45c7f32cb0ba8e
Peter Gilbert <petegilb@bu.edu>
2026-07-26 13:51:49 +0900

    win condition + fuse ui, sound implementation etc.

--- main.gd
+++ main.gd
@@ -8,12 +8,14 @@ signal input_success
 
 @onready var music_player: AudioStreamPlayer = $MusicPlayer
 @onready var sfx_player: AudioStreamPlayer = $SFXPlayer1
+@onready var end_music_player: AudioStreamPlayer = $EndMusicPlayer
 @onready var level = $Level1
 @onready var anim_player: AnimationPlayer = $Level1/AnimationPlayer
 @onready var bomb_sfx: AudioStreamPlayer2D = $Level1/%BombSounds
 @onready var ui = $Ui
 @onready var ui_rhythm = $Ui/Exclamation
 @onready var ui_rhythm_dest = $Ui/ExclamationShape
+@onready var ui_fuse: TextureProgressBar = $Ui/Fuse
 
 const UI_NOTE_SPEED = 1.0
 const INPUT_MARGIN = 0.08
@@ -28,14 +30,20 @@ var beat_duration = 0
 var last_beat = 0
 var next_beat_position = 0
 var current_song_position: float = 0.0
-var active_beat = -1
+var active_beat_position = -1
 var spawn_idx = 0
 
 # game state
+var game_over = false
 var num_success := 0
 var num_fail := 0
 const WIN_PERCENTAGE = 0.7
 var active_ui = {}
+var fuse = 100.0
+var fuse_speed = 1.0
+var fuse_pause = 0.0
+const MISS_PENALTY = 10.0
+const HIT_PAUSE = 0.5
 
 # sfx
 var disappointment: AudioStreamMP3 = preload("res://music/RhythmGame Disappointment.mp3")
@@ -43,7 +51,10 @@ var one_two_three: AudioStreamMP3 = preload("res://music/RhythmGame 1,2,3 Go!.mp
 var ok_sfx: AudioStreamMP3 = preload("res://music/RhythmGame Correct Input.mp3")
 var miss_sfx: AudioStreamMP3 = preload("res://music/RhythmGame Incorrect Input.mp3")
 var fall_sfx: AudioStream = preload("res://music/326133__wagna__falling.wav")
-var wick_sfx: AudioStream = preload("res://music/329045__scousemousejb__wick-of-detonation-bomb.wav")
+var explosion_sfx: AudioStream = preload("res://music/RhythmGame Explosion.mp3")
+var evil_laugh_sfx: AudioStream = preload("res://music/evil_laugh.wav")
+var woosh_sfx: AudioStream = preload("res://music/woosh.wav")
+var spin_sfx: AudioStream = preload("res://music/400685__supersnd__fidget-spinner1.wav")
 
 # referenced this video: https://www.youtube.com/watch?v=9XcLoEVnjrA
 
@@ -82,6 +93,7 @@ func set_song(input_song: AudioStreamMP3, song_path: String) -> void:
 # the higher the bpm, the shorter the beatDuration
 # this is the time for quarter notes (can divide further for eight, sixteenth, etc.)
 	beat_duration = 60/bpm
+	fuse_speed = 100.0 / input_song.get_length()
 	
 	# get the inputs from the chart created using https://alphros.itch.io/beatrice
 	var notes = song_data.charts[0].notes
@@ -90,11 +102,13 @@ func set_song(input_song: AudioStreamMP3, song_path: String) -> void:
 		active_input_beats.append(beat_float)
 		active_input_beats_sec.append(beat_float*beat_duration)
 		
-	print("set song to %s with bpm %d and beat duration %f" % [input_song.resource_path, bpm, beat_duration])
+	print("set song to %s with bpm %d and beat duration %f with fuse speed %f" \
+	 	% [input_song.resource_path, bpm, beat_duration, fuse_speed])
 	print(active_input_beats_sec)
 	
 # start the selected song (restarts if already playing something)
 func start_song():
+	end_music_player.stop()
 	music_player.stop()
 	if music_player.stream == null:
 		printerr("music player has no song to play!")
@@ -111,7 +125,12 @@ func start_song():
 	bomb.position += Vector3(0, 5, 0)
 	bomb.visible = true
 	
-func update_song():
+func update_song(delta: float):
+	if fuse <= 0.0:
+		music_player.stop()
+		song_ended()
+		return
+	
 	current_song_position = music_player.get_playback_position() + AudioServer.get_time_since_last_mix()
 	# Compensate for output latency. (https://docs.godotengine.org/en/stable/tutorials/audio/sync_with_audio.html)
 	current_song_position -= AudioServer.get_output_latency()
@@ -122,13 +141,12 @@ func update_song():
 		# TODO is this +1 correct?
 		var idx = spawn_idx
 		active_ui[idx] = new_ui_note
-		print(new_ui_note)
 		var tween = get_tree().create_tween()
-		print(ui_rhythm_dest.position)
 		var pos_diff = new_ui_note.position - ui_rhythm_dest.position
-		tween.tween_property(new_ui_note, "position", ui_rhythm_dest.position, UI_NOTE_SPEED)
+		var travel = active_input_beats_sec[idx] - current_song_position
+		tween.tween_property(new_ui_note, "position", ui_rhythm_dest.position, travel)
 		tween.tween_property(new_ui_note, "position", 
-			ui_rhythm_dest.position - Vector2(pos_diff.x,0), UI_NOTE_SPEED)
+			ui_rhythm_dest.position - Vector2(pos_diff.x,0), travel)
 		tween.tween_callback(func():
 			new_ui_note.queue_free()
 			active_ui.erase(idx)
@@ -146,11 +164,15 @@ func update_song():
 			print("no more inputs!")
 		
 	if current_song_position >= next_beat_position - INPUT_MARGIN:
-		if active_beat != next_beat_position:
-			active_beat = next_beat_position
+		if active_beat_position != next_beat_position:
+			active_beat_position = next_beat_position
 			enter_beat.emit()
-	elif active_beat > -1 and current_song_position >= active_beat + INPUT_MARGIN:
-		active_beat = -1
+	elif active_beat_position > -1 and current_song_position >= active_beat_position + INPUT_MARGIN:
+		var hit_idx = active_input_beats_sec.find(active_beat_position)
+		if active_ui[hit_idx].visible:
+			play_sfx(disappointment)
+			fuse -= MISS_PENALTY
+		active_beat_position = -1
 		exit_beat.emit()
 		
 	# update spinning record
@@ -158,29 +180,65 @@ func update_song():
 		var beats = current_song_position * (bpm / 60.0)
 		var rotations = beats / beats_per_rotation
 		record.rotation.y = -rotations * TAU
+		
+	# update fuse
+	if fuse_pause <= 0.0:
+		fuse -= fuse_speed * delta
+		ui_fuse.value = fuse
+	else:
+		fuse_pause -= delta
+		fuse_pause = clamp(fuse_pause, 0.0, HIT_PAUSE)
+		if fuse_pause == 0.0:
+			bomb_sfx.play()
+	
+	if Input.is_action_just_pressed("ui_left"):
+		music_player.stop()
+		song_ended()
 	
 	if Input.is_action_just_pressed("space"):
-		if active_beat != -1:
+		if active_beat_position != -1:
 			print("ok!")
 			input_success.emit()
-			play_sfx(ok_sfx)
+			play_sfx(ok_sfx, 0.5)
 			num_success += 1
-			var hit_idx = active_input_beats_sec.find(active_beat)
+			var hit_idx = active_input_beats_sec.find(active_beat_position)
 			if active_ui.has(hit_idx):
 				active_ui[hit_idx].visible = false
+				hit_pause()
 		else:
-			play_sfx(miss_sfx)
+			play_sfx(miss_sfx, 0.7)
 			print("miss")
 			num_fail += 1
 
 func song_ended() -> void:
+	game_over = true
+	bomb_sfx.stop()
 	var total_beats: int = active_input_beats.size()
 	var success_percentage = float(num_success) / float(total_beats)
 	print("final percentage %f" % success_percentage)
-	if success_percentage > WIN_PERCENTAGE:
+	if fuse > 0.0:
 		print("you win!")
+		play_sfx(spin_sfx)
+		var tween = get_tree().create_tween()
+		tween.tween_property(record, "rotation:y", -TAU * 10, 1.0).as_relative()
+		await tween.finished
+		anim_player.play("win")
+		play_sfx(woosh_sfx)
+		await anim_player.animation_finished
+		end_music_player.play()
+		print("show ui here.")
 	else:
+		record.rotation = Vector3(0, 0, 0)
+		anim_player.play("EXPLODE")
+		play_sfx(evil_laugh_sfx)
 		print("you lose! the bomb exploded!")
+		await anim_player.animation_finished
+		play_sfx(explosion_sfx)
+
+# if the player hits a note, pause the fuse momentarily.
+func hit_pause() -> void:
+	bomb_sfx.stop()
+	fuse_pause = HIT_PAUSE
 
 func _ready() -> void:
 	# get record
@@ -190,6 +248,9 @@ func _ready() -> void:
 	set_song(song1, song1_json_path)
 	start_song()
 
-func _process(_delta: float) -> void:
-	if music_player.is_playing():
-		update_song()
+func _process(delta: float) -> void:
+	if music_player.is_playing() and not game_over:
+		update_song(delta)
+	
+	#if we_won:
+		#record.rotation.y = -rotations * TAU
